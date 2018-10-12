a0865b5cf0c2cd45a48fd0cfbcb1a64f1f4d757f
Пользователь <@notebook>
2018-10-12 10:28:18 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
-
+#include <fstream>
 using namespace std;
 
 int main() {
-
+	ifstream in("input.txt");
+	while (in){
+		string line;
+		getline (in,line);
+		cout << line << endl;
+	}
 	return 0;
 }
