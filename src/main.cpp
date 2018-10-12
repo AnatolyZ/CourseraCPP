ee0501a771c67986073785eacfb06c7320d53997
Пользователь <@notebook>
2018-10-12 10:47:25 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 	ifstream in("input.txt");
 	string line;
+	cout << setprecision(3);
 	while (getline (in,line)){
 		cout << line << endl;
 	}
