b01abc5d938ec7069048a84ae3178444724b23d0
Пользователь <@notebook>
2018-10-12 17:11:27 +0300

    final

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
-
+#include <exception>
+#include <string>
 using namespace std;
 
+string AskTimeServer() {
+    //throw invalid_argument("");
+    return "12:12:12";
+}
+
+class TimeServer {
+public:
+    string GetCurrentTime() {
+    	try {
+    		LastFetchedTime = AskTimeServer();
+    	} catch(system_error& serr){
+    		cout << "System error";
+    	}
+    	return LastFetchedTime;
+    }
+
+private:
+    string LastFetchedTime = "00:00:00";
+};
+
 int main() {
-	int qn; // queries number
-	cin >> qn;
-	while (qn){
-		string cmd;
-		cin >> cmd;
-		if (cmd == ""){
-
-		}else if (cmd == " "){
-
-		}
-		--qn;
-	}
-	return 0;
+    // ћен€€ реализацию функции AskTimeServer, убедитесь, что это код работает корректно
+    TimeServer ts;
+    try {
+        cout << ts.GetCurrentTime() << endl;
+    } catch (exception& e) {
+        cout << "Exception got: " << e.what() << endl;
+    }
+    return 0;
 }
