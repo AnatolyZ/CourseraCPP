1735ed1ce0faf1878ff38045df97cdb12d2fb8e7
Пользователь <@notebook>
2018-10-12 12:22:48 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+struct Student{
+	string name;
+	string surname;
+	int day;
+	int month;
+	int year;
+};
+
 int main() {
+	vector<Student> students_list;
+	int sn; // students number
+	cin >> sn;
+	while (sn){
+		Student st;
+		cin >> st.name;
+		cin >> st.surname;
+		cin >> st.day;
+		cin >> st.month;
+		cin >> st.year;
+		students_list.push_back(st);
+		--sn;
+	}
 	int qn; // queries number
 	cin >> qn;
 	while (qn){
