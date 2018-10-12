596c0ebb905e8d6d7833e1aee8049f63ef8f38f5
Пользователь <@notebook>
2018-10-12 11:11:00 +0300

    ver1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
-
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-
+	ifstream input ("input.txt");
+	int rows;
+	input >> rows;
+	int columns;
+	input >> columns;
+	while (rows){
+		while (columns){
+			int v;
+			input >> v;
+			cout << setw(10) << v;
+			--columns;
+		}
+		cout << endl;
+		--rows;
+	}
 	return 0;
 }
