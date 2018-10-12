e0d3a357e32eee4c05ed73f3985b941f5976e6b0
Пользователь <@notebook>
2018-10-12 12:00:08 +0300

    queries template

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main() {
+	int qn; // queries number
+	cin >> qn;
+	while (qn){
+		string cmd;
+		if (cmd == ""){
 
+		}else if (cmd == " "){
+
+		}
+		--qn;
+	}
 	return 0;
 }
